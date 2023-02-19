85ea5fcc60c1eeae4db7ac945ea921c932aefc41
Sahil Maheshwari <sahilm1711@gmail.com>
2023-02-19 22:13:57 +0530

    trying npm

--- pages/home/home_1.jsx
+++ pages/home/home_1.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import {
 	Hero,
-	Browse_category,
+	Bids,
+	Top_collection,
+	Tranding_category,
+	NewseLatter,
 } from '../../components/component';
-import TrustedPartner from "../../components/dao/TrustedPartner";
-import Testimonial from "../../components/testimonial/Testimonial";
-import CoverflowCarousel from '../../components/carousel/CoverflowCarousel';
 import Meta from '../../components/Meta';
-import Cta from "../../components/cta/Cta";
 
 const Home_1 = () => {
 	return (
 		<main>
 			<Meta title="Home 1 || Xhibiter | NFT Marketplace Next.js Template" />
 			<Hero />
-			<Browse_category />
-			<TrustedPartner/>
-			<CoverflowCarousel/>
-			<Testimonial/>
-			<Cta/>
+			<Bids />
+			<Top_collection />
+			<Tranding_category />
+			<NewseLatter />
 		</main>
 	);
 };
