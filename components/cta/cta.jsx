053cdda61657ae78528a15183c02adfb92eec7ca
Sahil Maheshwari <sahilm1711@gmail.com>
2023-03-24 13:45:49 +0530

    Adding changes for the logo

--- components/cta/cta.jsx
+++ components/cta/cta.jsx
@@ -6,7 +6,7 @@ const Cta = () => {
 		<div>
 			{/* <!-- CTA --> */}
 			<div className="relative z-10 dark:bg-jacarta-900">
-				<div className="container">
+				<div className="container pt-10">
 					<div className="relative overflow-hidden rounded-2.5xl px-16 pt-16 pb-8 shadow-md lg:px-24">
 						<picture className="pointer-events-none absolute inset-0 -z-10 dark:hidden">
 							<img src="/images/gradient_light.jpg" alt="gradient" className="h-full w-full" />
@@ -17,11 +17,16 @@ const Cta = () => {
 						<div className="items-center justify-between md:flex">
 							<div className="mb-6 md:w-1/2">
 								<h2 className="mb-4 font-display text-2xl text-jacarta-700 dark:text-white sm:text-3xl">
-									Would you like some help getting started?
+									Need a consultation for your project ?
 								</h2>
 								<p className="mb-8 text-lg dark:text-jacarta-300">
-									Our support heroes will have you up and running in less than 10mins.
+									Book up a meet with me or send an email.
 								</p>
+								<Link href="mailto:sahilm1711@gmail.com">
+									<a className="inline-block rounded-full bg-accent py-3 px-8 text-center font-semibold text-white shadow-accent-volume transition-all hover:bg-accent-dark mr-2">
+										Send mail
+									</a>
+								</Link>
 								<Link href="/contact">
 									<a className="inline-block rounded-full bg-accent py-3 px-8 text-center font-semibold text-white shadow-accent-volume transition-all hover:bg-accent-dark">
 										Schedule a Call
--- components/header/Header01.jsx
+++ components/header/Header01.jsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import DarkMode from "../mode/DarkMode";
-import Logo from "./../../public/images/logo.png";
-import WhiteLogo from "./../../public/images/logo_white.png";
+import Logo from "./../../public/images/logo.svg";
+import WhiteLogo from "./../../public/images/logo_white.svg";
 import { v4 as uuidv4 } from "uuid";
 import { useRouter } from "next/router";
 import {
@@ -417,30 +417,6 @@ export default function Header01() {
             </a>
           </Link>
           {/* End  logo */}
-
-          <form
-            action="search"
-            className="relative ml-12 mr-8 hidden basis-3/12 lg:block xl:ml-[8%]"
-          >
-            <input
-              type="search"
-              className="text-jacarta-700 placeholder-jacarta-500 focus:ring-accent border-jacarta-100 w-full rounded-2xl border py-[0.6875rem] px-4 pl-10 dark:border-transparent dark:bg-white/[.15] dark:text-white dark:placeholder-white"
-              placeholder="Search"
-            />
-            <span className="absolute left-0 top-0 flex h-full w-12 items-center justify-center rounded-2xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width={24}
-                height={24}
-                className="fill-jacarta-500 h-4 w-4 dark:fill-white"
-              >
-                <path fill="none" d="M0 0h24v24H0z" />
-                <path d="M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z" />
-              </svg>
-            </span>
-          </form>
-          {/* End Desktop search form */}
 
           <div className="js-mobile-menu dark:bg-jacarta-800 invisible fixed inset-0 z-10 ml-auto items-center bg-white opacity-0 lg:visible lg:relative lg:inset-auto lg:flex lg:bg-transparent lg:opacity-100 dark:lg:bg-transparent">
             <nav className="navbar w-full">
