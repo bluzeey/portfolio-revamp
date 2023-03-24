757cd74426d701bbbf41045fad6f54560927c537
Sahil Maheshwari <sahilm1711@gmail.com>
2023-03-24 16:39:09 +0530

    Hire me , Work experience section

---
+++ data/work-ex_data.js
@@ -0,0 +1,22 @@
+const workex_data = [
+	{
+		id: '1',
+		company: 'Codevyasa',
+		image: '/images/work-ex/codevyasa.svg',
+		description:'',
+		text: 'Unique, fully 3D and built to unite the design multiverse. Designed and styled by Digimental.',
+		coverPhoto: '/images/user/banner.jpg',
+	},
+	{
+		id: ' avatar_8',
+		title: 'Superdo',
+		image: '/images/avatars/avatar_8.jpg',
+		icon: true,
+		userId: '0x7a86c0b064171007716bbd6af96676935799a63e',
+		joinYear: '2019',
+		text: 'Unique, fully 3D and built to unite the design multiverse. Designed and styled by Digimental.',
+		coverPhoto: '/images/user/banner.jpg',
+	},
+];
+
+export default workex_data;
