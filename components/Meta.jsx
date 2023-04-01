4027beba580e758734e682a68daf3983a89a9f1f
Sahil Maheshwari <sahilm1711@gmail.com>
2023-04-02 03:57:34 +0530

    Adding changes to the favicon

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -5,7 +5,7 @@ const Meta = ({ title, keyword, desc }) => {
     <div>
       <Head>
         <title>{title}</title>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/images/hero/hero.svg" />
         <meta name="description" content={desc} />
         <meta name="keyword" content={keyword} />
       </Head>
@@ -14,7 +14,7 @@ const Meta = ({ title, keyword, desc }) => {
 };
 
 Meta.defaultProps = {
-  title: "Xhibiter | NFT Marketplace Next.js Template",
+  title: "Sahil Maheshwari | Web Developer",
   keyword:
     "bitcoin, blockchain, crypto, crypto collectibles, crypto makretplace, cryptocurrency, digital items, market, nft, nft marketplace, nft next js, NFT react, non-fungible tokens, virtual asset, wallet",
   desc: "The world's first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital items.",
