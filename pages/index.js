8afe95af460d1a0f9aad861d1c39679ff44fdf2f
Sahil Maheshwari <sahilm1711@gmail.com>
2023-04-06 17:52:45 +0530

    Adding og tags and improving seo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect } from "react";
 import UserContext from "../components/UserContext";
 import Home_1 from "./home/home_1";
+import { NextSeo } from 'next-seo';
+
 
 export default function Home() {
   const { scrollRef } = useContext(UserContext);
@@ -18,6 +20,37 @@ export default function Home() {
 
   return (
     <>
+    <NextSeo
+      title="Sahil Maheshwari | Web Developer"
+      description="This is portfolio of Sahil Maheshwari , a web developer with 2+ years of experience."
+      canonical="https://sahilmaheshwari.com/"
+      openGraph={{
+        url: 'https://sahilmaheshwari.com/',
+        title: 'Sahil Maheshwari | Web Developer',
+        description: 'This is portfolio of Sahil Maheshwari , a web developer with 2+ years of experience."',
+        images: [
+          {
+            url: 'https://sahilmaheshwari.com/images/hero/hero.svg',
+            width: 800,
+            height: 600,
+            alt: 'My animated picture',
+            type: 'image/jpeg',
+          },
+          {
+            url: 'https://sahilmaheshwari.com/images/projects/googleSearch.svg',
+            width: 900,
+            height: 800,
+            alt: 'Search Application Project on  portfolio',
+            type: 'image/jpeg',
+          },
+        ],
+        site_name: 'sahilmaheshwari',
+      }}
+      twitter={{
+        handle: '@TalkinIdeas',
+        site: '@site',
+        cardType: 'summary_large_image',
+      }} />
       <Home_1 />
     </>
   );
