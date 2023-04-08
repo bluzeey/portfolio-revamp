afd83a16965a2e4d91a353008a01aebc568148ec
Sahil Maheshwari <sahilm1711@gmail.com>
2023-04-08 18:57:50 +0530

    Adding changes

--- data/testimonial_data.js
+++ data/testimonial_data.js
@@ -4,13 +4,13 @@ export const testimonial_data = [
 		desc: "I worked with Sahil on a website for my small business, and I couldn't be happier with the results. Sahil was easy to work with, understood my vision, and delivered a website that looks great and functions perfectly. I highly recommend Sahil for any web development needs",
 		id: '0General Partner at Entrepreneur',
 		name: 'Surya Jakhotia',
-		img: '/images/testimonials/SuryaJakhotia.jpeg',
+		img: '/images/testimonials/suryajakhotia.jpeg',
 	},
 	{
 		title: 'Founder at Assembo',
 		desc: "I needed a website for my new startup, and Sahil delivered exactly what I was looking for. He was professional, responsive, and worked tirelessly to make sure that every detail was perfect. I'm thrilled with the website, and I wouldn't hesitate to recommend Sahil to anyone looking for a talented web developer.",
 		id: '1General Partner at Entrepreneur',
 		name: 'Lan Li',
-		img: '/images/testimonials/LanLi.jpeg',
+		img: '/images/testimonials/lanli.jpeg',
 	}
 ];
