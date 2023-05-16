db9dc9bc808dc2534374b3492cac9ec5b6bf22e1
Sahil Maheshwari <sahilm1711@gmail.com>
2023-05-16 20:11:28 +0530

    Adding changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	productionBrowserSourceMaps: true,
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en',
-	}
+	images: {
+        unoptimized: true
+    }
 };
 
 module.exports = nextConfig;
